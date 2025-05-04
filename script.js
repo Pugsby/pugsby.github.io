75bc82fe84af1565641b73d823852296480ab7b7
Pugsby <pugsbypuggerson@duck.com>
2025-05-03 21:57:56 -0500

    new clicky sounds :3

--- script.js
+++ script.js
@@ -10,3 +10,33 @@ function loadContent(dir) {
         });
 }
 loadContent('panelContents/about.html');
+
+function attachHoverSound() {
+    document.querySelectorAll('button').forEach(button => {
+        if (!button.hasAttribute('data-hover-sound')) {
+            button.setAttribute('data-hover-sound', 'true');
+            button.addEventListener('mouseover', () => {
+                const audio = new Audio('hover.mp3');
+                audio.play();
+            });
+            button.addEventListener('click', () => {
+                const audio = new Audio('click.mp3');
+                audio.play();
+            });
+        }
+    });
+}
+
+attachHoverSound();
+
+const observer = new MutationObserver(() => {
+    attachHoverSound();
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
+
+function playCarSoundAndRemoveImage() {
+    const audio = new Audio('car.mp3');
+    audio.play();
+    document.getElementById('car').remove();
+}
